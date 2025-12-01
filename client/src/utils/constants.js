4008d03ab7be1bc3e17a53b911e166e8046e647d
MThuyet <mthuyet294@gmail.com>
2025-12-01 09:07:52 +0700

    chore: update nodemailer to version 4.4.7 and change API root URL for production

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -5,7 +5,7 @@ if (process.env.BUILD_MODE === 'dev') {
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'http://api.mthuyet.site:30760'
+  apiRoot = 'https://mthuyet-trello-api.onrender.com'
 }
 
 export const ACTION_UPDATE_CARD_MEMBERS = {
